24882462e02cb3008cc254e9350f6647980c5c95
yunming <yunming.li@outlook.com>
2018-09-25 14:05:22 +0800

    init version

---
+++ probset6.py
@@ -0,0 +1,25 @@
+import sys
+import argparse
+
+
+
+
+
+def get_args():
+    parser=argparse.ArgumentParser()
+    parser.add_argument('--algoNum',type=str,default='1',help='Algorithm Type')
+
+
+
+
+
+
+
+
+
+
+
+
+
+if __name__ == "__main__":
+
