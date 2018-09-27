34af47bbe71fd8682fcc29d3b6772560692d1df1
Yunming LI <yunming.li@singtel.com>
2018-09-27 15:28:04 +0800

    Add hash string function

--- hashFunction.py
+++ hashFunction.py
@@ -1,6 +1,5 @@
 import math
 
-
 class hashFunction:
     def __init__(self,a,b,p,M):
         self.a=a
@@ -8,6 +7,18 @@ class hashFunction:
         self.p=p
         self.M=M
 
-    def hash(self,x):
-        result = ((self.a*x+self.b)%self.p)%self.M
+    def hash(self,x)
+        #using same logic of java.lang.string hashCode function to convert string to int
+        #https://en.wikipedia.org/wiki/Java_hashCode()
+        if isinstance(x,int):
+            result = ((self.a*x+self.b)%self.p)%self.M
+        else:
+            #Convert to String
+            x=str(x)
+            #Convert x to integer
+            str_to_int=0
+            for i in range(0,len(x)):
+                str_to_int+=x[i]*math.pow(31,len(x)-1-i)
+            #Use hash function to hash integer
+            result = ((self.a * str_to_int + self.b) % self.p) % self.M
         return result
